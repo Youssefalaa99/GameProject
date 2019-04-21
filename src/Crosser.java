b1deef158935bf31fbc47578a241f9ddeb56d0fc
Youssefalaa99 <youssef_alaa99@yahoo.com>
2019-04-21 21:40:51 +0200

    updated source files

---
+++ src/Crosser.java
@@ -0,0 +1,40 @@
+import java.awt.image.BufferedImage;
+
+public class Crosser implements ICrosser {
+
+
+    @Override
+    public boolean canSail() {
+        return false;
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public int getEatingRank() {
+        return 0;
+    }
+
+    @Override
+    public BufferedImage[] getImages() {
+        return new BufferedImage[0];
+    }
+
+    @Override
+    public ICrosser makeCopy() {
+        return null;
+    }
+
+    @Override
+    public void setLabelToBeShown(String label) {
+
+    }
+
+    @Override
+    public String getLabelToBeShown() {
+        return null;
+    }
+}
---
+++ src/LevelOne.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class LevelOne implements ICrossingStrategy {
+
+
+    @Override
+    public boolean isValid(List<ICrosser> rightBankCrossers, List<ICrosser> leftBankCrossers, List<ICrosser> boatRiders) {
+        return false;
+    }
+
+    @Override
+    public List<ICrosser> getInitialCrossers() {
+        return null;
+    }
+
+    @Override
+    public String[] getInstructions() {
+        return new String[0];
+    }
+}
